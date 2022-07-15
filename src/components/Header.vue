928a005db86c48d8f135d0bc0df4e312da0d7f8f
lucas.quevedo <lucas.quevedo@arba.gov.ar>
2022-07-15 15:39:38 -0300

    Se instalaron los plugins

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <router-link to="/">ecommerce</router-link>
+      <div class="d-flex">
+        <input
+          v-model="busqueda"
+          class="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button
+          @click="fetchProductos({ nombre: busqueda })"
+          class="btn btn-outline-success"
+          type="button"
+        >
+          Search
+        </button>
+      </div>
+      <div>
+        <button
+          @click="$router.push('/favoritos')"
+          type="button"
+          class="btn btn-secondary btn-sm me-2"
+        >
+          <i class="bi bi-heart-fill"></i> {{ cantidadFavoritos }}
+        </button>
+        <button
+          @click="$router.push('/carrito')"
+          type="button"
+          class="btn btn-secondary btn-sm"
+        >
+          <i class="bi bi-cart-fill"></i> {{ cantidadProductosCarrito }}
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+export default {
+  name: "Header-app",
+  data() {
+    return {
+      busqueda: null,
+    };
+  },
+  computed: {
+    ...mapGetters(["cantidadProductosCarrito", "cantidadFavoritos"]),
+  },
+  methods: {
+    ...mapActions(["fetchProductos"]),
+  },
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
 
-Vue.config.productionTip = false
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+Vue.prototype.$http = axios;
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
